36a7bb9294d52bf88273e8339c9daeb3022ba10c
kubusiaq <sactarek@gmail.com>
2023-12-05 15:33:20 +0100

    progress in day 5 part 1

---
+++ day5/solution.py
@@ -0,0 +1,15 @@
+# so, for the first we need to get our components into lists, 
+"""
+the first thing is the seeds
+then, we have several maps, we are checking, if our one is in one of them, and in every, then we note the final plant, and choose min element from the final list
+"""
+import re
+with open('sample.txt', 'r') as f:
+    seeds = f.readline()[7:].split(' ')
+    soils = []
+    a = ""
+    while not bool(re.match(r"[\d \d \d]", a)):
+        a = f.readline()
+        print(a)
+
+ 
