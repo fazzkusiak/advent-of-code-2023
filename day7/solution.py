6429b461511c2c84c6758686c4233db2105c9cf2
kubusiaq <sactarek@gmail.com>
2023-12-08 20:53:16 +0100

    day 7 progress

---
+++ day7/solution.py
@@ -0,0 +1,13 @@
+with open('sample.txt', 'r') as f:
+    hands = []
+    bids = []
+    line = f.readline()
+    print(line)
+
+    hands = [line.rstrip[5:]]
+
+    
+
+
+    print(hands)
+    print(bids)
